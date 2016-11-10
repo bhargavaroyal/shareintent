4418fdf8cbf3aa555afac089755e55f602f10628
bhargavaroyal <bgugamshetty@office1.welldoc-communications.com>
2016-11-10 10:25:04 +0530

    share intent

---
+++ app/src/main/java/com/bhargavaroyal/shareintent/MainActivity.java
@@ -0,0 +1,77 @@
+package com.bhargavaroyal.shareintent;
+
+import java.io.File;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.app.Activity;
+import android.content.Intent;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // listeners of our two buttons
+        View.OnClickListener handler = new View.OnClickListener() {
+            public void onClick(View v) {
+                switch (v.getId()) {
+
+                    case R.id.buttonShareTextUrl:
+                        shareTextUrl();
+                        break;
+
+                    case R.id.buttonShareImage:
+                        shareImage();
+                        break;
+                }
+            }
+        };
+
+        // our buttons
+        findViewById(R.id.buttonShareTextUrl).setOnClickListener(handler);
+        findViewById(R.id.buttonShareImage).setOnClickListener(handler);
+
+    }
+
+    // Method to share either text or URL.
+    private void shareTextUrl() {
+        Intent share = new Intent(android.content.Intent.ACTION_SEND);
+        share.setType("text/plain");
+        share.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+
+        // Add data to the intent, the receiving app will decide
+        // what to do with it.
+        share.putExtra(Intent.EXTRA_SUBJECT, "Title Of The Post");
+        share.putExtra(Intent.EXTRA_TEXT, "http://www.codeofaninja.com");
+
+        startActivity(Intent.createChooser(share, "Share link!"));
+    }
+
+
+    // Method to share any image.
+    private void shareImage() {
+        Intent share = new Intent(Intent.ACTION_SEND);
+
+        // If you want to share a png image only, you can do:
+        // setType("image/png"); OR for jpeg: setType("image/jpeg");
+        share.setType("image/*");
+
+        // Make sure you put example png image named myImage.png in your
+        // directory
+        String imagePath = Environment.getExternalStorageDirectory()
+                + "/myImage.png";
+
+        File imageFileToShare = new File(imagePath);
+
+        Uri uri = Uri.fromFile(imageFileToShare);
+        share.putExtra(Intent.EXTRA_STREAM, uri);
+
+        startActivity(Intent.createChooser(share, "Share Image!"));
+    }
+
+}
